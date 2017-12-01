60ed8b80663d8c41192b96b0f652573686bd3305
Karl Atkinson <karl.atkinson@gmail.com>
2017-12-01 14:27:25 +0000

    fix, without tests

--- Serilog.Sinks.Network/Formatters/LogstashJsonFormatter.cs
+++ Serilog.Sinks.Network/Formatters/LogstashJsonFormatter.cs
@@ -21,58 +21,62 @@ using Serilog.Formatting.Json;
 
 namespace Serilog.Sinks.Network.Formatters
 {
-  public class LogstashJsonFormatter : ITextFormatter
-  {
-    private static readonly JsonValueFormatter ValueFormatter = new JsonValueFormatter();
-    
-    public void Format(LogEvent logEvent, TextWriter output)
+    public class LogstashJsonFormatter : ITextFormatter
     {
-      FormatContent(logEvent, output);
-      output.WriteLine();
+        private static readonly JsonValueFormatter ValueFormatter = new JsonValueFormatter();
+        private static string _precedingDelimiter;
+
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            _precedingDelimiter = "";
+            FormatContent(logEvent, output);
+            output.WriteLine();
+        }
+
+        private static void FormatContent(LogEvent logEvent, TextWriter output)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            output.Write('{');
+
+            WritePropertyAndValue(output, "timestamp", logEvent.Timestamp.ToString("o"));
+            WritePropertyAndValue(output, "level", logEvent.Level.ToString());
+            WritePropertyAndValue(output, "message", logEvent.MessageTemplate.Render(logEvent.Properties));
+
+            if (logEvent.Exception != null)
+            {
+                WritePropertyAndValue(output, "exception", logEvent.Exception.ToString());
+            }
+
+            WriteProperties(logEvent.Properties, output);
+
+            output.Write('}');
+        }
+
+        private static void WritePropertyAndValue(TextWriter output, string propertyKey, string propertyValue)
+        {
+            output.Write(_precedingDelimiter);
+            _precedingDelimiter = ",";
+
+            JsonValueFormatter.WriteQuotedJsonString(propertyKey, output);
+            output.Write(":");
+            JsonValueFormatter.WriteQuotedJsonString(propertyValue, output);
+        }
+
+        private static void WriteProperties(IReadOnlyDictionary<string, LogEventPropertyValue> properties,
+            TextWriter output)
+        {
+            foreach (var property in properties)
+            {
+                output.Write(_precedingDelimiter);
+                _precedingDelimiter = ",";
+
+                var camelCasePropertyKey = property.Key[0].ToString().ToLower() + property.Key.Substring(1);
+                JsonValueFormatter.WriteQuotedJsonString(camelCasePropertyKey, output);
+                output.Write(':');
+                ValueFormatter.Format(property.Value, output);
+            }
+        }
     }
-
-    private static void FormatContent(LogEvent logEvent, TextWriter output)
-    {
-      if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
-      if (output == null) throw new ArgumentNullException(nameof(output));
-
-      output.Write('{');
-
-      WritePropertyAndValue(output, "timestamp", logEvent.Timestamp.ToString("o"));
-      WritePropertyAndValue(output, "level", logEvent.Level.ToString());
-      WritePropertyAndValue(output, "message", logEvent.MessageTemplate.Render(logEvent.Properties));
-      
-      if (logEvent.Exception != null)
-      {
-        WritePropertyAndValue(output, "exception", logEvent.Exception.ToString());
-      }
-      
-      WriteProperties(logEvent.Properties, output);
-      
-      output.Write('}');
-    }
-
-    private static void WritePropertyAndValue(TextWriter output, string propertyKey, string propertyValue)
-    {
-      JsonValueFormatter.WriteQuotedJsonString(propertyKey, output);
-      output.Write(":");
-      JsonValueFormatter.WriteQuotedJsonString(propertyValue, output);
-      output.Write(",");
-    }
-
-    private static void WriteProperties(IReadOnlyDictionary<string, LogEventPropertyValue> properties, TextWriter output)
-    {
-      var precedingDelimiter = "";
-      foreach (var property in properties)
-      {
-        output.Write(precedingDelimiter);
-        precedingDelimiter = ",";
-
-        var camelCasePropertyKey = property.Key[0].ToString().ToLower() + property.Key.Substring(1);
-        JsonValueFormatter.WriteQuotedJsonString(camelCasePropertyKey, output);
-        output.Write(':');
-        ValueFormatter.Format(property.Value, output);
-      }
-    }
-  }
 }
